edf66298330430c75df7120ea22494e2377576fc
Anubhav Narayan Singh <anubhavsingh2018694@gmail.com>
2025-03-10 23:08:04 +0530

    Modified code

--- week-7/server/database/index.js
+++ week-7/server/database/index.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.ObjectId;
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://anubhav860102:anubhav9936@cluster0.vfhuo.mongodb.net/assignment-7');
-        console.log('Connected to MongoDB successfully');
+        await mongoose.connect(process.env.MONGO_DB_URL);
+        console.log("Successfully connected to db");
     } catch (error) {
         console.error('Error connecting to MongoDB:', error.message);
         process.exit(1);
@@ -11,54 +11,15 @@ const connectDB = async () => {
 };
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    purchasedCourses: [{ 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Course' 
-    }]
+    
 });
 
 const adminSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+    
 });
 
 const courseSchema = new mongoose.Schema({
-    title: {
-        type: String, 
-        required: true,
-    },
-    price: {
-        type: Number,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    imageUrl: {
-        type: String,
-        required: true,
-    },
-    published: {
-        type: Boolean, 
-        default: false
-    }
+    
 });
 
 const User = mongoose.model('User', userSchema);
